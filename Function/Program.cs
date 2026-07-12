6d30e68a769aebf3e5ed3269a480a5336e439a65
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-11 20:23:21 -0700

    build-in functions

--- Function/Program.cs
+++ Function/Program.cs
@@ -4,11 +4,29 @@
     {
         static void Main(string[] args)
         {
+            //built-in functions call 
+            // className.functionName(parameters)
+
+            Math.Cos(90);
+            Math.Pow(2, 3);    // 2^3 = 8
+            Console.WriteLine("hello");
+            Console.Clear();
+
+            double r = Math.Cos(90);
+            Console.WriteLine(r); 
+
+            Console.WriteLine(Math.Pow(2, 3));
+
+            /////////////////
+
             Console.WriteLine("Enter a number: ");
             float userInput = float.Parse(Console.ReadLine());
 
             double result = Math.Cos(userInput);
             Console.WriteLine(result);
+
+            /////////////////
+            
 
         }
     }
