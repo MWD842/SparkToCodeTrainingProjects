55512aff688b7b77e3070e379c66d496a0164605
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-12 10:37:49 +0400

    DateTime Now, Today

--- Function/Program.cs
+++ Function/Program.cs
@@ -7,32 +7,40 @@
             //built-in functions call 
             // className.functionName(parameters)
 
-            Math.Cos(90);
-            Math.Pow(2, 3);    // 2^3 = 8
-            Console.WriteLine("hello");
-            Console.Clear();
+            //Math.Cos(90);
+            //Math.Pow(2, 3);    // 2^3 = 8
+            //Console.WriteLine("hello");
+            //Console.Clear();
 
-            double r = Math.Cos(90);
-            Console.WriteLine(r); 
+            //double r = Math.Cos(90);
+            //Console.WriteLine(r); 
 
-            Console.WriteLine(Math.Pow(2, 3));
+            //Console.WriteLine(Math.Pow(2, 3));
 
             /////////////////
 
-            Console.WriteLine("Enter a number: ");
-            float userInput = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a number: ");
+            //float userInput = float.Parse(Console.ReadLine());
 
-            double result = Math.Cos(userInput);
-            Console.WriteLine(result);
+            //double result = Math.Cos(userInput);
+            //Console.WriteLine(result);
 
             /////////////////
             
-            Console.WriteLine("Enter base number: ");
-            float baseNumber = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter power number: ");
-            float powerNumber = float.Parse(Console.ReadLine());
-            double result1= Math.Pow(baseNumber, powerNumber);
-            Console.WriteLine(result1);
+            //Console.WriteLine("Enter base number: ");
+            //float baseNumber = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter power number: ");
+            //float powerNumber = float.Parse(Console.ReadLine());
+            //double result1= Math.Pow(baseNumber, powerNumber);
+            //Console.WriteLine(result1);
+
+            /////////////////
+            
+            DateTime currentTiming = DateTime.Now; 
+            Console.WriteLine(currentTiming);
+
+            DateTime timing = DateTime.Today;
+            Console.WriteLine(timing);
         }
     }
 }
